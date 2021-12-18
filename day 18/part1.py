7a71c7ff1165129983dcea54c3751e8efd19ef97
PaoDerDoktor <PaoDerDoktor@gmail.com>
2021-12-18 16:01:11 +0100

    WIP - Day 18

---
+++ day 18/part1.py
@@ -0,0 +1,134 @@
+from __future__ import annotations
+from math import floor, ceil, inf
+
+class BinNode:
+    def __init__(self, left: int|list, right: int|list, depth: int = 0, prev: BinNode|None = None):
+        if type(left) == int:
+            self.left: int = left # type: ignore -- Already tested in `if-else` statement right before
+        else:
+            self.left: BinNode = BinNode(left[0], left[1], depth+1, self) # type: ignore -- Already tested in `if-else` statement right before
+            self.left.prev = self # type: ignore - avoiding None conflict
+            self.left.depth = depth+1
+            
+        if type(right) == int:
+            self.right: int = right # type: ignore -- Already tested in `if-else` statement right before
+        else:
+            self.right: BinNode = BinNode(right[0], right[1], depth+1, self) # type: ignore -- Already tested in `if-else` statement right before
+        
+        self.prev:  BinNode|None = prev
+        self.depth: int          = depth
+    
+    def __repr__(self) -> str:
+        return f"[{repr(self.left)}, {repr(self.right)}]"
+    
+    def __eq__(self, otherBinNode: object) -> bool:
+        if isinstance(otherBinNode, self.__class__):
+            return (self.left == otherBinNode.left
+                    and self.right == otherBinNode.right)
+        return False
+    
+    def __ne__(self, otherBinNode: object) -> bool:
+        return not self == otherBinNode
+    
+    def _make_explode(self) -> bool:
+        if self.depth == 4:
+            currentNode: BinNode|None = self.prev
+            previousNode: BinNode = self
+            left:  int = self.left  # type: ignore
+            right: int = self.right # type: ignore --- We know at this depth that left and right can't be sublists
+            isLeftAdded: bool = False
+            isRightAdded: bool = False
+            while not currentNode == None:
+                if currentNode.left != previousNode and not isLeftAdded:
+                    if type(currentNode.left) == int:
+                        currentNode.left += left # type: ignore --- Again, pylance not checking conditions :/
+                    else:
+                        nextNode = currentNode.left
+                        while type(nextNode.right) != int:
+                            nextNode = nextNode.right
+                        nextNode.right += left # type: ignore --- We know nextNode.right is an int because of the while just before
+                    isLeftAdded = True
+                if currentNode.right != previousNode and not isRightAdded:
+                    if type(currentNode.right) == int:
+                        currentNode.right += right # type: ignore --- Again, pylance not checking conditions :/
+                    else:
+                        nextNode = currentNode.right
+                        while type(nextNode.left) != int:
+                            nextNode = nextNode.left
+                        nextNode.left += right # type: ignore --- We know nextNode.left is an int because of the while just before
+                    isRightAdded = True
+                previousNode = currentNode
+                currentNode = currentNode.prev
+            if self.prev.left == self:  # type: ignore --- avoiding None conflict
+                self.prev.left = 0  # type: ignore --- wtf pylance
+            if self.prev.right == self: # type: ignore --- avoiding None conflict
+                self.prev.right = 0 # type: ignore --- wtf pylance
+            return True
+        return False
+                    
+    def explode(self) -> bool:
+        path = [_ for _ in (self.right, self.left) if type(_) != int]
+        while len(path) > 0:
+            currentNode = path.pop()
+            if currentNode._make_explode():
+                return True
+            path += [_ for _ in (currentNode.right, currentNode.left) if type(_) != int]
+        return False
+
+    def split(self) -> bool:
+        path = [self]
+        while len(path) > 0:
+            currentNode = path.pop()
+            if type(currentNode.left) == int and currentNode.left >= 10: # type: ignore --- PYLANCE.....
+                currentNode.left = BinNode(floor(currentNode.left/2), ceil(currentNode.left/2), currentNode.depth+1, currentNode) # type: ignore --- I really need to understand how `|` work
+                return True
+            if type(currentNode.right) == int and currentNode.right >= 10: # type: ignore --- PYLANCE.....
+                currentNode.right = BinNode(floor(currentNode.right/2), ceil(currentNode.right/2), currentNode.depth+1, currentNode) # type: ignore --- I really need to understand how `|` work
+                return True
+            path += [_ for _ in (currentNode.right, currentNode.left) if type(_) != int]
+        return False
+    
+    def reduce(self):
+        didSomething: bool = True
+        while didSomething:
+            didSomething = False
+            didSomething = self.explode()
+            if didSomething:
+                continue
+            didSomething = self.split()
+    
+    def get_magnitude(self) -> int:
+        if type(self.left) == int and type(self.right) != int:
+            return 3*self.left + 2*self.right.get_magnitude() # type: ignore --- pylance
+        if type(self.left) != int and type(self.right) == int:
+            return 3*self.left.get_magnitude() + 2*self.right # type: ignore --- pylance
+        if type(self.left) == int and type(self.right) == int:
+            return 3*self.left + 2*self.right # type: ignore --- pylance
+        else:
+            return 3*self.left.get_magnitude() + 2*self.right.get_magnitude()
+            
+        
+    
+    
+def load(numberStr: str):
+    return eval(numberStr)
+
+def list_to_tree(num: list, depth: int = 0, prev: None|BinNode = None) -> BinNode:
+    return BinNode(num[0], num[1], depth, prev)
+
+def day18_part1_main() -> int:
+    with open("day 18/inputs.txt", 'r') as inFile:
+        numList = [load(_.strip()) for _ in inFile.readlines()]
+        
+        print("first print =",numList)
+        node: BinNode = list_to_tree(numList.pop())
+        node.reduce()
+        while len(numList) > 0:
+            node = list_to_tree(load(f"[{repr(node)}, {repr(numList.pop(0))}]"))
+            node.reduce()
+            
+        return node.get_magnitude()
+        
+        
+if __name__ == "__main__":
+    print(day18_part1_main())
---
+++ day 18/test_day18.py
@@ -0,0 +1,178 @@
+from part1 import BinNode, load, list_to_tree
+
+# --- PART 1
+
+def test_load_simple():
+    assert load('[1,2]') == [1,2]
+
+def test_load_simple_nested():
+    assert load('[[1,2],3]') == [[1,2],3]
+    assert load('[9,[8,7]]') == [9,[8,7]]
+    assert load('[[1,9],[8,5]]') == [[1,9],[8,5]]
+    
+def test_load_multi_nested():
+    assert load('[[[[1,2],[3,4]],[[5,6],[7,8]]],9]') == [[[[1,2],[3,4]],[[5,6],[7,8]]],9]
+    assert load('[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]') == [[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]
+    assert load('[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]') == [[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]
+
+def test_list_to_tree_simple():
+    node: BinNode = list_to_tree(load('[1,2]'))
+    assert node.left == 1
+    assert node.right == 2
+    assert node.prev == None
+    assert node.depth == 0
+    
+def test_simple_binNode_equality():
+    node1: BinNode = list_to_tree(load('[1,2]'))
+    node2: BinNode = list_to_tree(load('[1,2]'))
+    
+    assert node1 == node2
+    
+def test_simple_binNode_inequality():
+    node1: BinNode = list_to_tree(load('[1,2]'))
+    node2: BinNode = list_to_tree(load('[1,3]'))
+    node3: BinNode = list_to_tree(load('[2,2]'))
+    
+    assert node1 != node2
+    assert node1 != node3
+    assert node2 != node3
+    
+def test_list_to_tree_nested():
+    node: BinNode = list_to_tree(load('[1,[2,[3,4]]]'))
+    assert node.left == 1
+    assert node.right == list_to_tree(load('[2,[3,4]]'))
+    assert node.right.left == 2
+    assert node.right.right == list_to_tree(load('[3,4]'))
+    assert node.right.right.left == 3
+    assert node.right.right.right == 4
+    
+    assert node.prev == None
+    assert node.depth == 0
+    assert node.right.prev == node
+    assert node.right.depth == 1
+    assert node.right.right.prev == node.right
+    assert node.right.right.depth == 2
+    
+def test_nested_binNode_equality():
+    assert list_to_tree(load('[1,[2,3]]')) == list_to_tree(load('[1,[2,3]]'))
+    assert list_to_tree(load('[1,[2,[3,4]]]')) == list_to_tree(load('[1,[2,[3,4]]]'))
+    assert list_to_tree(load('[[0,1],[2,[3,4]]]')) == list_to_tree(load('[[0,1],[2,[3,4]]]'))
+    
+def test_nested_binNode_inequality():
+    node1 = list_to_tree(load('[1,[2,3]]'))
+    node2 = list_to_tree(load('[1,[4,3]]'))
+    node3 = list_to_tree(load('[1,[2,[0,1]]]'))
+    
+    assert node1 != node2
+    assert node1 != node3
+    assert node2 != node3
+
+def test_explode_happening():
+    assert list_to_tree(load('[[[[[9,8],1],2],3],4]')).explode() == True
+    
+def test_explode():
+    node1: BinNode = list_to_tree(load('[[[[[9,8],1],2],3],4]'))
+    node1.explode()
+    assert node1 == list_to_tree(load('[[[[0,9],2],3],4]'))
+    
+    node2: BinNode = list_to_tree(load('[7,[6,[5,[4,[3,2]]]]]'))
+    node2.explode()
+    assert node2 == list_to_tree(load('[7,[6,[5,[7,0]]]]'))
+    
+    node3: BinNode = list_to_tree(load('[[6,[5,[4,[3,2]]]],1]'))
+    node3.explode()
+    assert node3 == list_to_tree(load('[[6,[5,[7,0]]],3]'))
+    
+    node4: BinNode = list_to_tree(load('[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]'))
+    node4.explode()
+    assert node4 == list_to_tree(load('[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]'))
+    
+    node5: BinNode = list_to_tree(load('[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]'))
+    node5.explode()
+    assert node5 == list_to_tree(load('[[3,[2,[8,0]]],[9,[5,[7,0]]]]'))
+    
+def test_split_happening():
+    assert list_to_tree(load('[[[[0,7],4],[15,[0,13]]],[1,1]]')).split() == True
+    
+def test_split():
+    node1: BinNode = list_to_tree(load('[[[[0,7],4],[15,[0,13]]],[1,1]]'))
+    node1.split()
+    assert node1 == list_to_tree(load('[[[[0,7],4],[[7,8],[0,13]]],[1,1]]'))
+
+    node2: BinNode = list_to_tree(load('[[[[0,7],4],[[7,8],[0,13]]],[1,1]]'))
+    node2.split()
+    assert node2 == list_to_tree(load('[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]'))
+    
+def test_reduce():
+    node: BinNode = list_to_tree(load('[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]'))
+    node.reduce()
+    assert node == list_to_tree(load('[[[[0,7],4],[[7,8],[6,0]]],[8,1]]'))
+    
+def test_magnitude():
+    node1: BinNode = list_to_tree(load('[[1,2],[[3,4],5]]'))
+    node1.reduce()
+    assert node1.get_magnitude() == 143
+    
+    node2: BinNode = list_to_tree(load('[[[[0,7],4],[[7,8],[6,0]]],[8,1]]'))
+    node2.reduce()
+    assert node2.get_magnitude() == 1384
+    
+    node3: BinNode = list_to_tree(load('[[[[1,1],[2,2]],[3,3]],[4,4]]'))
+    node3.reduce()
+    assert node3.get_magnitude() == 445
+    
+    node4: BinNode = list_to_tree(load('[[[[3,0],[5,3]],[4,4]],[5,5]]'))
+    node4.reduce()
+    assert node4.get_magnitude() == 791
+    
+    node5: BinNode = list_to_tree(load('[[[[5,0],[7,4]],[5,5]],[6,6]]'))
+    node5.reduce()
+    assert node5.get_magnitude() == 1137
+    
+    node6: BinNode = list_to_tree(load('[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]'))
+    node6.reduce()
+    assert node6.get_magnitude() == 3488
+    
+def test_part1_aoc_official_example():
+    with open("day 18/test.txt", 'r') as inFile:
+        numList = [load(_.strip()) for _ in inFile.readlines()]
+        
+        node: BinNode = list_to_tree(numList.pop(0))
+        assert node == list_to_tree(load('[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]'))
+        node.reduce()
+        
+        print("step 1")
+        nodeToAdd: BinNode = list_to_tree(numList.pop(0))
+        assert nodeToAdd == list_to_tree(load('[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]'))
+        node = list_to_tree(load(f"[{repr(node)}, {repr(nodeToAdd)}]"))
+        node.reduce()
+        assert node == list_to_tree(load('[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]'))
+        
+        print("step 2")
+        nodeToAdd = list_to_tree(numList.pop(0))
+        assert nodeToAdd == list_to_tree(load('[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]'))
+        node = list_to_tree(load(f"[{repr(node)}, {repr(nodeToAdd)}]"))
+        node.reduce()
+        assert node == list_to_tree(load('[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]'))
+        
+        print("step 3")
+        nodeToAdd = list_to_tree(numList.pop(0))
+        assert nodeToAdd == list_to_tree(load('[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]'))
+        node = list_to_tree(load(f"[{repr(node)}, {repr(nodeToAdd)}]"))
+        print(node)
+        node.reduce()
+        print(node)
+        assert node == list_to_tree(load('[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]'))
+
+def test_addition():
+    numList = [load(_) for _ in ['[[[[4,3],4],4],[7,[[8,4],9]]]', '[1,1]']]
+    
+    node: BinNode = list_to_tree(numList.pop(0))
+    assert node == list_to_tree(load('[[[[4,3],4],4],[7,[[8,4],9]]]'))
+    node.reduce()
+    nodeToAdd: BinNode = list_to_tree(numList.pop(0))
+    assert nodeToAdd == list_to_tree(load('[1,1]'))
+    node = list_to_tree(load(f"[{repr(node)}, {repr(nodeToAdd)}]"))
+    assert node == list_to_tree(load('[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]'))
+    node.reduce()
+    assert node == list_to_tree(load('[[[[0,7],4],[[7,8],[6,0]]],[8,1]]'))
